85c5eae662bd7cb7bc338f88dcff1b64f0d10f7e
Qiulin Duan <t-qiulinduan@microsoft.com>
2022-07-22 12:40:28 +0800

    upsert to google

--- GoogleCalendarConsoleSample/Program.cs
+++ GoogleCalendarConsoleSample/Program.cs
@@ -9,6 +9,7 @@ using System;
 using System.IO;
 using System.Threading;
 using Newtonsoft.Json.Linq;
+using System.Data.Common;
 
 namespace CalendarQuickstart
 {
@@ -34,9 +35,11 @@ namespace CalendarQuickstart
             GetGoogleCalendarEvents(accessToken);*/
 
             // 3.
-            /*var accessToken = UpdateAccessToken().Result;*/
-            var accessToken = "ya29.A0AVA9y1t7IUYIJDojAR2aty2gnO3GnmsorWhDw0GGfRLYCWeOaWBdtqO9skEdQztKaNuEishfKr-eTX7aCo_zO6F2dYWk7HRmyifXcerpR-jhSBQ757xIzKAn8jppw1LRV89beFrzyiOdxZVtI8zEWSZHD3ofsAYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4MVdxWThIVGxmdWI2OTNhcnVHOWRYdw0165";
-            GetGoogleCalendarEvents(accessToken);
+
+            var accessToken = UpdateAccessToken().Result;
+            // var accessToken = "ya29.A0AVA9y1t7IUYIJDojAR2aty2gnO3GnmsorWhDw0GGfRLYCWeOaWBdtqO9skEdQztKaNuEishfKr-eTX7aCo_zO6F2dYWk7HRmyifXcerpR-jhSBQ757xIzKAn8jppw1LRV89beFrzyiOdxZVtI8zEWSZHD3ofsAYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4MVdxWThIVGxmdWI2OTNhcnVHOWRYdw0165";
+
+            TestGoogleCalendarEvents(accessToken);
 
         }
 
@@ -99,7 +102,7 @@ namespace CalendarQuickstart
             return refreshedAccessToken;
         }
 
-        static void GetGoogleCalendarEvents(string accessToken)
+        static void TestGoogleCalendarEvents(string accessToken)
         {
             GoogleCredential cred = GoogleCredential.FromAccessToken(accessToken);
 
@@ -109,7 +112,9 @@ namespace CalendarQuickstart
                 HttpClientInitializer = cred
             });
 
-            // Define parameters of request.
+
+            /*==================================== Get Event List. ====================================*/
+            /*// Define parameters of request.
             EventsResource.ListRequest request = service.Events.List("primary");
             request.TimeMin = DateTime.Now;
             request.ShowDeleted = false;
@@ -136,7 +141,69 @@ namespace CalendarQuickstart
                 DateTime originDateTime = DateTime.SpecifyKind(eventItem.Start.DateTime.Value, DateTimeKind.Unspecified);
                 var startTime = TimeZoneInfo.ConvertTimeToUtc(originDateTime, System.TimeZoneInfo.Utc);
                 Console.WriteLine("{0} ({1}) ({2})", eventItem.Summary, when, whenTimeZone);
-            }
+            }*/
+
+
+            /*==================================== Get Calendar Id. ====================================*/
+            /*var newCalendar = new Calendar()
+            {
+                Summary = "TakeLessons",
+                TimeZone = "America/Los_Angeles"
+            };
+
+            CalendarsResource.InsertRequest insertCalendarRequest = service.Calendars.Insert(newCalendar);
+            Calendar calendar = insertCalendarRequest.Execute();
+            String calendarId = calendar.Id;*/
+
+            string calendarId = "u64c8l0ivsuiiqtq8j0qeviabs@group.calendar.google.com";
+
+
+            /*==================================== Insert Event. ====================================*/
+            Event newEvent = new Event()
+            {
+                Summary = "Google I/O 2015",
+                Location = "800 Howard St., San Francisco, CA 94103",
+                Description = "A chance to hear more about Google's developer products.",
+                Start = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2022-07-22T09:00:00-07:00"),
+                    TimeZone = "America/Los_Angeles",
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2022-07-22T17:00:00-07:00"),
+                    TimeZone = "America/Los_Angeles",
+                },
+                Recurrence = new String[] { "RRULE:FREQ=DAILY;COUNT=2" },
+                Attendees = new EventAttendee[] {
+                    new EventAttendee() { Email = "lpage@example.com" },
+                    new EventAttendee() { Email = "sbrin@example.com" },
+                },
+                Reminders = new Event.RemindersData()
+                {
+                    UseDefault = false,
+                    Overrides = new EventReminder[] {
+                        new EventReminder() { Method = "email", Minutes = 24 * 60 },
+                        new EventReminder() { Method = "sms", Minutes = 10 },
+                    }
+                }
+            };
+
+            /*EventsResource.InsertRequest insertRequest = service.Events.Insert(newEvent, calendarId);
+            Event createdEvent = insertRequest.Execute();
+            Console.WriteLine("Event created: {0}, eventId: {1}", createdEvent.HtmlLink, createdEvent.Id);*/
+
+
+            /*==================================== Update Event. ====================================*/
+            string eventId = "f37lq998oehqolk53b5gcpipc0";
+            newEvent.Summary = "Google I/O 2022";
+            EventsResource.UpdateRequest updateRequest = service.Events.Update(newEvent, calendarId, eventId);
+            Event result =  updateRequest.Execute();
+            Console.WriteLine("Event updated: {0}, eventId: {1}", result.HtmlLink, result.Id);
+
+
+            /*==================================== Delete Event. ====================================*/
+            service.Events.Delete(calendarId, result.Id).Execute();
         }
     }
 }
